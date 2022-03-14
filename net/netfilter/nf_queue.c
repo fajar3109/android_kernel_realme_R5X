a461d0858a1c095738ae2ab16e7209015bebecdc
HenryS <18201042@ittelkom-pwt.ac.id>
2022-03-14 06:36:48 +0000

    Revert "netfilter: nf_queue: fix possible use-after-free"
    This reverts commit ef97921ccdc243170fcef857ba2a17cf697aece5.

--- net/netfilter/nf_queue.c
+++ net/netfilter/nf_queue.c
@@ -91,12 +91,9 @@ void nf_queue_entry_release_refs(struct nf_queue_entry *entry)
 EXPORT_SYMBOL_GPL(nf_queue_entry_release_refs);
 
 /* Bump dev refs so they don't vanish while packet is out */
-bool nf_queue_entry_get_refs(struct nf_queue_entry *entry)
+void nf_queue_entry_get_refs(struct nf_queue_entry *entry)
 {
 	struct nf_hook_state *state = &entry->state;
-
-	if (state->sk && !refcount_inc_not_zero(&state->sk->sk_refcnt))
-		return false;
 
 	if (state->in)
 		dev_hold(state->in);
@@ -116,7 +113,6 @@ bool nf_queue_entry_get_refs(struct nf_queue_entry *entry)
 			dev_hold(physdev);
 	}
 #endif
-	return true;
 }
 EXPORT_SYMBOL_GPL(nf_queue_entry_get_refs);
 
@@ -204,11 +200,7 @@ static int __nf_queue(struct sk_buff *skb, const struct nf_hook_state *state,
 		.size	= sizeof(*entry) + afinfo->route_key_size,
 	};
 
-	if (!nf_queue_entry_get_refs(entry)) {
-		kfree(entry);
-		return -ENOTCONN;
-	}
-
+	nf_queue_entry_get_refs(entry);
 	afinfo->saveroute(skb, entry);
 	status = qh->outfn(entry, queuenum);
 
